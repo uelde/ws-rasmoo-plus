963128c8609655b6f0217ba133dafcb2fb6af92b
uelde <usazevedo.dev@gmail.com>
2025-03-23 14:41:09 -0300

    first commit

---
+++ src/test/java/com/cliente/ws/rasmoo_plus/WsRasmooPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliente.ws.rasmoo_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsRasmooPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
